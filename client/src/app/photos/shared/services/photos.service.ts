ba1a90113646b086eb19c7768f8fcf19c3b900d1
sandrine <sandrinezocca@gmail.com>
2020-10-08 15:39:58 +0200

    Getting a full response http Post request

--- client/src/app/photos/shared/services/photos.service.ts
+++ client/src/app/photos/shared/services/photos.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Photo } from '../models/photo.model';
-
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +10,23 @@ export class PhotosService {
 
   constructor(private http: HttpClient, ) { }
 
-  public searchPhotos(photo: Photo): Observable<Photo> {
-    return this.http.post<Photo>('api/photos', photo);
+  public searchPhotos(photo: Photo): Observable<any> {
+
+    const httpOptions: { headers; observe; responseType } = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      }),
+      observe: 'response',
+      responseType: 'json'
+    };
+
+    return this.http.post<Photo>('api/photos', photo, httpOptions).pipe(
+      tap( resp => {
+        console.log(resp.body.results);
+        return resp.body.results
+        }
+      )
+    )
   }
+  
 }
--- client/src/app/photos/shared/store/photos.selectors.ts
+++ client/src/app/photos/shared/store/photos.selectors.ts
@@ -2,4 +2,5 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { PhotosState } from './photos.reducer';
 
 export const photosSelector = createFeatureSelector('photos');
-export const filterSelector = createSelector(photosSelector, (photosState: PhotosState) => photosState.photo);
+//export const filterSelector = createSelector(photosSelector, (photosState: PhotosState) => photosState.photo);
+export const photosResultsSelector = createSelector(photosSelector, (photosState: PhotosState) => photosState.photo);
